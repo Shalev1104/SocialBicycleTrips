6ab3d7d32c97892d9cffe2653839195ab3d86b7f
Shalev1104 <Shalev0125@gmail.com>
2020-04-08 03:36:10 +0300

    SplashScreen

--- Model/obj/Debug/90/android/environment.x86_64.s
+++ Model/obj/Debug/90/android/environment.x86_64.s
@@ -69,7 +69,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"6c7414b1-695a-4b54-86f3-b88940e6f86a"
+	.asciz	"2a2ac00b-8c8d-4cf6-977b-62fd17748a4f"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
--- SocialBicycleTrips/MainActivity.cs
+++ SocialBicycleTrips/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.Widget;
 
 namespace SocialBicycleTrips
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
